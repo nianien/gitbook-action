5cad99f676ee7177a6b7bbdfa6d5671e330a1df4
liyifei@kuaishou.com <liyifei@kuaishou.com>
2025-07-07 03:35:15 +1000

    for hint

--- plugins/gitbook-plugin-hint/index.js
+++ plugins/gitbook-plugin-hint/index.js
@@ -3,14 +3,17 @@ var format = require('util').format;
 module.exports = {
     blocks: {
         hint: {
-            process: function(block) {
+            process: function (block) {
                 var style = block.kwargs.style || 'info';
                 var title = block.kwargs.title || style.charAt(0).toUpperCase() + style.slice(1);
+                var content = (this.renderBlock && typeof this.renderBlock === 'function')
+                    ? this.renderBlock(block.body)
+                    : block.body;
                 return format(
                     '<div class="hint %s"><p class="hint-title">%s</p>\n%s\n</div>',
                     style,
                     title,
-                    this.renderBlock(block.body)
+                    content
                 );
             }
         }
